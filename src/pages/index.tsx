1e97a6184e01bbeb8bcbb443a04a7cba78660435
Valigton Verissimo <40119609+valigton@users.noreply.github.com>
2021-04-23 17:42:03 -0300

    Created SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,21 @@
-import { Header } from "../components/Header";
+import { useEffect } from "react"
 
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <h1>Index</h1>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
